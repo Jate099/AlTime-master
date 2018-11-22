419fec6c0389ae72eb0c617b2c91e3a8d6758e26
Jate099 <42478567+Jate099@users.noreply.github.com>
2018-11-22 13:39:00 -0500

    Timer

---
+++ app/src/main/java/com/example/altime/altime/PerfilFragment.java
@@ -0,0 +1,38 @@
+package com.example.altime.altime;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class PerfilFragment extends Fragment {
+
+    View vista;
+
+    Button timer;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        vista = inflater.inflate(R.layout.fragment_perfil, container,false);
+
+        timer = vista.findViewById(R.id.timer);
+
+        timer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction frag = getFragmentManager().beginTransaction();
+                frag.replace(R.id.fragment_contenedor, new TimerFragment());
+                frag.commit();
+            }
+        });
+
+        return vista;
+    }
+}
